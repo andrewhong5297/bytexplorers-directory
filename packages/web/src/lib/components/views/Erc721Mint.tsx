faa23973dd696a5bedabed1bb0d6a634260e9437
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2024-06-12 00:33:54 -0400

    shorter

--- packages/web/src/lib/components/views/Erc721Mint.tsx
+++ packages/web/src/lib/components/views/Erc721Mint.tsx
@@ -63,7 +63,7 @@ export function Erc721Mint({ tokenContract }: { tokenContract?: TokenConfig }) {
                   <ContractLink contract={tokenContract} />
                 </div>
                 <div className="mb-2"> 
-                  {"Join hundreds of crypto data analysts, engineers, and scientists as we participate together in quests, building up onchain reputation through our multi-token reward system."}
+                  {"Join hundreds of crypto data analysts, engineers, and scientists in quests. You'll build up onchain reputation through our multi-token reward system."}
                 </div>
                 <div className="mb-10"> 
                   <a href="https://dune.com/cryptodatabytes/bytexplorers" target="_blank" style={{ color: '#DD4850', textDecoration: 'underline' }}>Read more about the community</a>
